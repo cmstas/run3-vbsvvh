c96cb892af2f974027de750836cbaa1cb9464408
aashayarora <aaarora@ucsd.edu>
2025-04-01 12:57:44 -0700

    clean commented out lines, will add them as we go

--- preselection/src/main.cpp
+++ preselection/src/main.cpp
@@ -4,22 +4,26 @@
 #include "weights.h"
 #include "corrections.h"
 #include "utils.h"
+
+#include "selections_ZeroLep3FJ.h"
 #include "selections_OneLep2FJ.h"
 
 #include "argparser.hpp"
 
 struct MyArgs : public argparse::Args {
     std::string &spec = kwarg("i,input", "spec.json path");
+    std::string &ana = kwarg("a,ana", "Tag of analyzer to use for event selection");
     int &nthreads = kwarg("n,nthreads", "number of threads").set_default(1);
     bool &cutflow = flag("cutflow", "print cutflow");
-    bool &JMS = flag("jms", "JMS");
-    bool &JMR = flag("jmr", "JMR");
-    bool &METUnclustered = flag("met", "MET unclustered");
-    std::string &ana = kwarg("a,ana", "Tag of analyzer to use for event selection");
-    std::string &jec = kwarg("jec", "JEC").set_default("");
-    std::string &output = kwarg("o,output", "output root file").set_default("");
-    std::string &variation = kwarg("var", "variation").set_default("nominal");
     std::string &cut = kwarg("cut", "cut on final snapshot").set_default("");
+    std::string &output = kwarg("o,output", "output root file").set_default("");
+
+    // uncertainty flags, will uncomment as we add them
+    // std::string &jec = kwarg("jec", "JEC").set_default("");
+    // bool &METUnclustered = flag("met", "MET unclustered");
+    // bool &JMS = flag("jms", "JMS");
+    // bool &JMR = flag("jmr", "JMR");
+    // std::string &variation = kwarg("var", "variation").set_default("nominal");
 };
 
 
@@ -27,8 +31,11 @@ RNode runAnalysis(RNode df, MyArgs args) {
 
     std::cout << " -> Run " << args.ana << "::runAnalysis()" << std::endl;
 
-    if (args.ana == "OneLep2FJ") {
-        return OneLep2FJ::runAnalysis(df);
+    if (args.ana == "ZeroLep3FJ") {
+        return ZeroLep3FJ::runPreselection(df);
+    }
+    else if (args.ana == "OneLep2FJ") {
+        return OneLep2FJ::runPreselection(df);
     }
     else{
         std::cerr << "Did not recognize analysis namespace: " << args.ana  << std::endl;
@@ -47,25 +54,7 @@ RNode runDataAnalysis(RNode df_, MyArgs args) {
 RNode runMCAnalysis(RNode df_, MyArgs args) {
     // corrections
     auto df = defineCorrectedCols(df_);
-    
-    // apply pre preselection corrections
-    
-    // df = METPhiCorrections(cset_met_2016preVFP, cset_met_2016postVFP, cset_met_2017, cset_met_2018, df1);
-    // df = JetEnergyResolution(cset_jerc_2016preVFP, cset_jerc_2016postVFP, cset_jerc_2017, cset_jerc_2018, cset_jer_smear, df1, args.variation);
-    // } else if (args.METUnclustered) {
-    //     df = METUnclusteredCorrections(df1, args.variation);
-    // } else if (!args.jec.empty()) {
-    //     df = JetEnergyCorrection(cset_jerc_2016preVFP, cset_jerc_2016postVFP, cset_jerc_2017, cset_jerc_2018, df1, args.jec, args.variation);
-    // }
-
     df = runAnalysis(df, args);
-
-    // if (args.JMS) {
-    //     df_event = JMS_Corrections(cset_jms, df_event, args.variation);
-    // } else if (args.JMR) {
-    //     df_event = JMR_Corrections(cset_jmr, df_event, args.variation);
-    // }
-    
     //df = applyMCWeights(df); //FIXME: causing segfault
 
     return df;
@@ -120,16 +109,7 @@ int main(int argc, char** argv) {
     }
 
     // Run analysis
-    ROOT::RDF::RNode df_final = (isData) ? runDataAnalysis(df, args) : runMCAnalysis(df, args);
-
-    // Optionally print out cutflow
-    if (args.cutflow) {
-        if (args.ana == "OneLep2FJ") {
-            std::vector<std::string> cuts = {"passCut1", "passCut2", "passCut3", "passCut4", "passCut5", "passCut6", "passCut7", "passCut8", "passCut9", "passCut8_cr", "passCut9_cr"};
-            auto cutflow = Cutflow(df_final, cuts);
-            cutflow.Print(output_dir + "/" + output_file + "_cutflow.txt");
-        }
-    }
+    auto df_final = (isData) ? runDataAnalysis(df, args) : runMCAnalysis(df, args);
 
     // Optionally filter events
     if (!args.cut.empty()){
