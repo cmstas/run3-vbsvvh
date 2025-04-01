8fea5d81eeb03723600096d27e84cf4114de75c9
aashayarora <aaarora@ucsd.edu>
2025-04-01 12:58:40 -0700

    working on 1 and 0 lep channels

--- preselection/src/selections_ZeroLep3FJ.h
+++ preselection/src/selections_ZeroLep3FJ.h
@@ -1,5 +1,5 @@
-#ifndef SELECTIONS_ONELEP_H
-#define SELECTIONS_ONELEP_H
+#ifndef SELECTIONS_ZEROLEP_H
+#define SELECTIONS_ZEROLEP_H
 
 #include "ROOT/RDataFrame.hxx"
 
@@ -7,11 +7,13 @@
 
 using RNode = ROOT::RDF::RNode;
 
-namespace OneLep2FJ {
+namespace ZeroLep3FJ {
+
   RNode TriggerSelections(RNode df);
   RNode LeptonSelections(RNode df);
   RNode EventSelections(RNode df);
   RNode runPreselection(RNode df_);
-} // OneLep2FJ
+
+}
 
 #endif // SELECTIONS_H
