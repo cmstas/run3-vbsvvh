daf41ba80ddbc7ba1843eb791b4a5ab944f857df
aashayarora <aaarora@ucsd.edu>
2025-08-26 07:57:02 -0700

    fix model path

--- preselection/src/main.cpp
+++ preselection/src/main.cpp
@@ -87,7 +87,7 @@ int main(int argc, char** argv) {
     // Create output directory
     std::string output_dir = setOutputDirectory(args.ana, args.output_subdir);
 
-    SPANet::SPANetInference spanet_inference("spanet/spanet_assign_1p0_detect_1p0_v1.onnx", args.batch_size);
+    SPANet::SPANetInference spanet_inference("spanet/v1/model.onnx", args.batch_size);
 
     // add debugging
     if (args.debug) {
