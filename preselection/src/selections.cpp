a79700fa371ebf1e386b0b243bccc27ebdece7e5
aashayarora <aaarora@ucsd.edu>
2026-01-28 13:56:14 -0800

    fix veto filter, should be all true to veto.

--- preselection/src/selections.cpp
+++ preselection/src/selections.cpp
@@ -94,7 +94,7 @@ RNode AK4JetsSelection(RNode df_)
                                         "((is2016 && Jet_jetId >= 1) || (!is2016 && Jet_jetId >= 2))");
     
     df = applyJetVetoMaps(df);
-    df = df.Filter("Sum(Jet_vetoMap) == 0"); // for 2022EE+, events with any jet in veto region are removed
+    df = df.Filter("Sum(Jet_vetoMap) == Jet_pt.size()"); // for 2022EE+, events with any jet in veto region are removed
     df = df.Redefine("_good_ak4jets", "_good_ak4jets && !Jet_vetoMap");
 
     df = df.Define("Jet_isTightBTag", isbTagTight, {"year", "Jet_btagUParTAK4B"})
