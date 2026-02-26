762d021fc7b335d779f9b9a6aec2b4572536e59f
Kelci Mohrman <kelci.mohrman@cern.ch>
2026-02-25 18:33:32 -0800

    Clean up wrappers

--- preselection/run_rdf.py
+++ preselection/run_rdf.py
@@ -4,6 +4,19 @@ import os
 from datetime import datetime
 import subprocess
 
+SKIM_CHANNELS = [
+    "all_events",
+    "0lep_0FJ",
+    "0lep_1FJ",
+    "0lep_2FJ",
+    "0lep_3FJ",
+    "1lep_1FJ",
+    "2lep_1FJ",
+    "2lep_2FJ",
+    "2lepSS",
+    "3lep",
+    "4lep",
+]
 
 # Merge the input jsons into one dictionary
 def merge_jsons(input_paths_lst):
@@ -60,14 +73,17 @@ def main():
     # Set up the command line parser
     parser = argparse.ArgumentParser()
     parser.add_argument('jsons', nargs='+',    help = 'Input json file(s) containing files and metadata')
+    parser.add_argument('-a', '--channel',     help = 'Which analysis selection channel to run', choices=SKIM_CHANNELS)
     parser.add_argument('-m', '--mode',        help = 'Which mode to run in (local or condor)', choices=['local','condor'])
-    parser.add_argument('-o', '--output-path', help = 'Output directory')
-    parser.add_argument('-n', '--output-name', help = 'Output name')
+    parser.add_argument('-o', '--outpath',     help = 'Output directory', default=".")
+    parser.add_argument('-n', '--outname',     help = 'Output name', default="rdf_output")
     parser.add_argument('-r', '--run',         help = 'Which run (2 or 3)', choices=['2','3'])
     parser.add_argument('-p', '--prefix',      help = 'Prefix to append to the file paths', default=None)
     parser.add_argument('-j', '--n-cores',     help = 'Number of cores to use for local execution')
     parser.add_argument('-d', '--dry-run',     help = 'Do not actually execute the run command', action='store_true')
     args = parser.parse_args()
+
+    print(f"\nRunning for {args.outname}...")
 
     # Merge the input jsons
     merged_json_dict = merge_jsons(args.jsons)
@@ -79,20 +95,26 @@ def main():
     # Dump the merged content to a json file in merged_jsons/
     if not os.path.exists("merged_jsons"): os.mkdir("merged_jsons")
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-    merged_json_name = os.path.join("merged_jsons",f"merged_{args.output_name}_{timestamp}.json")
-    print(f"\nWriting merged file \"{merged_json_name}\"")
+    merged_json_name = os.path.join("merged_jsons",f"merged_{args.outname}_{timestamp}.json")
+    print(f"  -> Writing merged file \"{merged_json_name}\"")
     with open(merged_json_name, 'w') as outfile:
         json.dump(merged_json_dict, outfile, indent=4)
 
+    # Make an output directory out of outpath/outname
+    outdir = os.path.join(args.outpath,args.outname)
+    if not os.path.isdir(outdir): os.mkdir(outdir)
+    print(f"  -> RDF output will be located in: {outdir}")
+
     # Construct the bash run command
     if args.mode == "local":
-        command = f"bin/runAnalysis -i {merged_json_name} -o {args.output_path} -a 1Lep2FJ -j 8 --run_number {args.run}"
-        print(f"Running command \"{command}\"...\n")
+        command = f"bin/runAnalysis -i {merged_json_name} -o {outdir} -a {args.channel} -j {args.n_cores} --run_number {args.run}"
+        print(f"  -> Now running command \"{command}\"...\n")
         if not args.dry_run: os.system(command)
     elif args.mode == "condor":
-        command = f"python condor/submit.py -i {merged_json_name} -o {args.output_path} -a {args.output_name}"
-        print(f"Running command \"{command}\"...\n")
-        if not args.dry_run: os.system(command)
+        # TODO how to pass output dir to the condor run script
+        command = f"python condor/submit.py -c {merged_json_name} -o {outdir} -a {args.channel} --run_number {args.run}"
+        print(f"  -> Running command \"{command}\"...\n")
+        #if not args.dry_run: os.system(command)
 
     print("Done!")
 
--- preselection/run_wrapper.sh
+++ preselection/run_wrapper.sh
@@ -4,20 +4,24 @@
 PREFIX="/ceph/cms/"
 #PREFIX="root://redirector.t2.ucsd.edu:1095//"
 
+# Define an output dir, e.g. on UAF could be:
+OUT_DIR="/data/userdata/$USER/vbs_vvh_rdf/"
+mkdir $OUT_DIR
+
 # Run over a single file locally (for testing)
-#python run_rdf.py etc/input_sample_jsons/sig_sm/all_events/UL17_VBSWZH_VBSCuts_13TeV.json --prefix $PREFIX -n r2_sig_sm -m local -r 2
+#python run_rdf.py etc/input_sample_jsons/sig_sm/all_events/2017_VBSWZH_VBSCuts_13TeV.json --prefix $PREFIX -o $OUT_DIR -n test_small -a all_events -m local -r 2 -j 1
 
 # Run at scale over signal
-python run_rdf.py etc/input_sample_jsons/sig_sm/all_events --prefix $PREFIX -n r2_sig_sm -m condor -r 2
+python run_rdf.py etc/input_sample_jsons/sig_sm/all_events/ -p $PREFIX -o $OUT_DIR -n sigsm -a all_events -m condor -r 2
 
 # Run at scale over the 10 sets of data/bkg skims
-python run_rdf.py etc/input_sample_jsons/bkg/0lep_0FJ etc/input_sample_jsons/data/0lep_0FJ --prefix $PREFIX -n r2_0lep_0FJ -m condor -r 2
-python run_rdf.py etc/input_sample_jsons/bkg/0lep_1FJ etc/input_sample_jsons/data/0lep_1FJ --prefix $PREFIX -n r2_0lep_1FJ -m condor -r 2
-python run_rdf.py etc/input_sample_jsons/bkg/0lep_2FJ etc/input_sample_jsons/data/0lep_2FJ --prefix $PREFIX -n r2_0lep_2FJ -m condor -r 2
-python run_rdf.py etc/input_sample_jsons/bkg/0lep_3FJ etc/input_sample_jsons/data/0lep_3FJ --prefix $PREFIX -n r2_0lep_3FJ -m condor -r 2
-python run_rdf.py etc/input_sample_jsons/bkg/1lep_1FJ etc/input_sample_jsons/data/1lep_1FJ --prefix $PREFIX -n r2_1lep_1FJ -m condor -r 2
-python run_rdf.py etc/input_sample_jsons/bkg/2lep_1FJ etc/input_sample_jsons/data/2lep_1FJ --prefix $PREFIX -n r2_2lep_1FJ -m condor -r 2
-python run_rdf.py etc/input_sample_jsons/bkg/2lep_2FJ etc/input_sample_jsons/data/2lep_2FJ --prefix $PREFIX -n r2_2lep_2FJ -m condor -r 2
-python run_rdf.py etc/input_sample_jsons/bkg/2lepSS   etc/input_sample_jsons/data/2lepSS   --prefix $PREFIX -n r2_2lepSS   -m condor -r 2 # DNE yet
-python run_rdf.py etc/input_sample_jsons/bkg/3lep     etc/input_sample_jsons/data/3lep     --prefix $PREFIX -n r2_3lep     -m condor -r 2
-python run_rdf.py etc/input_sample_jsons/bkg/4lep     etc/input_sample_jsons/data/4lep     --prefix $PREFIX -n r2_4lep     -m condor -r 2
+python run_rdf.py etc/input_sample_jsons/bkg/0lep_0FJ etc/input_sample_jsons/data/0lep_0FJ -p $PREFIX -o $OUT_DIR -n r2_0lep_0FJ -a 0lep_0FJ -m condor -r 2
+python run_rdf.py etc/input_sample_jsons/bkg/0lep_1FJ etc/input_sample_jsons/data/0lep_1FJ -p $PREFIX -o $OUT_DIR -n r2_0lep_1FJ -a 0lep_1FJ -m condor -r 2
+python run_rdf.py etc/input_sample_jsons/bkg/0lep_2FJ etc/input_sample_jsons/data/0lep_2FJ -p $PREFIX -o $OUT_DIR -n r2_0lep_2FJ -a 0lep_2FJ -m condor -r 2
+python run_rdf.py etc/input_sample_jsons/bkg/0lep_3FJ etc/input_sample_jsons/data/0lep_3FJ -p $PREFIX -o $OUT_DIR -n r2_0lep_3FJ -a 0lep_3FJ -m condor -r 2
+python run_rdf.py etc/input_sample_jsons/bkg/1lep_1FJ etc/input_sample_jsons/data/1lep_1FJ -p $PREFIX -o $OUT_DIR -n r2_1lep_1FJ -a 1lep_1FJ -m condor -r 2
+python run_rdf.py etc/input_sample_jsons/bkg/2lep_1FJ etc/input_sample_jsons/data/2lep_1FJ -p $PREFIX -o $OUT_DIR -n r2_2lep_1FJ -a 2lep_1FJ -m condor -r 2
+python run_rdf.py etc/input_sample_jsons/bkg/2lep_2FJ etc/input_sample_jsons/data/2lep_2FJ -p $PREFIX -o $OUT_DIR -n r2_2lep_2FJ -a 2lep_2FJ -m condor -r 2
+python run_rdf.py etc/input_sample_jsons/bkg/2lepSS   etc/input_sample_jsons/data/2lepSS   -p $PREFIX -o $OUT_DIR -n r2_2lepSS   -a 2lepSS   -m condor -r 2 # DNE yet
+python run_rdf.py etc/input_sample_jsons/bkg/3lep     etc/input_sample_jsons/data/3lep     -p $PREFIX -o $OUT_DIR -n r2_3lep     -a 3lep     -m condor -r 2
+python run_rdf.py etc/input_sample_jsons/bkg/4lep     etc/input_sample_jsons/data/4lep     -p $PREFIX -o $OUT_DIR -n r2_4lep     -a 4lep     -m condor -r 2
