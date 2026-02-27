134798bd8c7de98648094c8f61b1c69d5431bb53
Kelci Mohrman <kelci.mohrman@cern.ch>
2026-02-27 12:02:48 -0800

    Update sig paths

--- preselection/run_wrapper.sh
+++ preselection/run_wrapper.sh
@@ -9,12 +9,12 @@ OUT_DIR="/data/userdata/$USER/vbs_vvh_rdf/"
 mkdir $OUT_DIR
 
 # Run over a single file locally (for testing sig, bkg, data)
-#python run_rdf.py etc/input_sample_jsons/sig_sm/all_events/2017_VBSWZH_VBSCuts_13TeV.json --prefix $PREFIX -o $OUT_DIR -n test_small -a all_events -m local -r 2 -j 1
-#python run_rdf.py etc/input_sample_jsons/bkg/1lep_1FJ/2018_WZTo1L1Nu2Q_4f_TuneCP5_13TeV.json --prefix $PREFIX -o $OUT_DIR -n test_small -a 1lep_1FJ -m condor -r 2 -j 1
+#python run_rdf.py etc/input_sample_jsons/sig_c2v1p0_c3_1p0/all_events/2017_VBSWZH_c2v1p0_c3_1p0.json --prefix $PREFIX -o $OUT_DIR -n test_small -a all_events -m local -r 2 -j 1
+#python run_rdf.py etc/input_sample_jsons/bkg/1lep_1FJ/2018_WZTo1L1Nu2Q_4f_TuneCP5_13TeV.json --prefix $PREFIX -o $OUT_DIR -n test_small -a 1lep_1FJ -m local -r 2 -j 1
 #python run_rdf.py etc/input_sample_jsons/data/0lep_2FJ/2016postVFP_JetHT_Run2016G-UL2016_NanoAODv15-v1_NANOAOD.json --prefix $PREFIX -o $OUT_DIR -n test_small -a 0lep_2FJ -m local -r 2 -j 1
 
 # Run at scale over signal
-python run_rdf.py etc/input_sample_jsons/sig_sm/all_events/ -p $PREFIX -o $OUT_DIR -n sigsm -a all_events -m condor -r 2
+python run_rdf.py etc/input_sample_jsons/sig_c2v1p0_c3_1p0/all_events/ -p $PREFIX -o $OUT_DIR -n sigsm -a all_events -m condor -r 2
 
 # Run at scale over the 10 sets of data/bkg skims
 python run_rdf.py etc/input_sample_jsons/bkg/0lep_0FJ etc/input_sample_jsons/data/0lep_0FJ -p $PREFIX -o $OUT_DIR -n r2_0lep_0FJ -a 0lep_0FJ -m condor -r 2
