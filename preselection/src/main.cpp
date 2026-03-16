323ac12ec9c5e2277947f860e4cb6a95c860c3f0
Kelci Mohrman <kelci.mohrman@cern.ch>
2026-03-15 21:11:33 -0700

    Turn on duplicate removal

--- preselection/src/main.cpp
+++ preselection/src/main.cpp
@@ -176,6 +176,7 @@ int main(int argc, char** argv) {
         df = runAnalysis(df, args.ana, args.run_number, isSignal, spanet_inference, spanet_inference_run2, args.runSPANetInference);
         df = applyDataWeights(df);
         df = applyDataCorrections(df);
+        df = removeDuplicates(df);
     } else {
         std::cout << " -> Running MC analysis" << std::endl;
         df = runAnalysis(df, args.ana, args.run_number, isSignal, spanet_inference, spanet_inference_run2, args.runSPANetInference, makeSpanetTrainingdata);
